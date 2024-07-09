ae71dd70d42900b8824b56dcc55e06a244feda72
David Wu <vladylo98@gmail.com>
2024-07-09 18:41:06 +0800

    first commit

---
+++ src/main/java/com/tia102test/emp/model/Emp.java
@@ -0,0 +1,21 @@
+package com.tia102test.emp.model;
+
+public class Emp {
+	private Integer empno;
+	private String ename;
+	
+	public Integer getEmpno() {
+		return empno;
+	}
+	public void setEmpno(Integer empno) {
+		this.empno = empno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	
+	
+}
